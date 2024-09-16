74da9ec99a170fecae7c7702c5cb67717612ce97
scoobyhax <128953410+scoobyhax@users.noreply.github.com>
2024-09-16 13:58:32 -0400

    uploaded unzipped deep freeze files

---
+++ 1.bat
@@ -0,0 +1,4 @@
+cd C:\Program Files (x86)\Faronics\Deep Freeze\Install C-0\ 
+mofcomp FaronicsProduct.mof >> %TEMP%\mof.log
+mofcomp FaronicsProduct_v2.mof >> %TEMP%\mof.log
+mofcomp DeepFreezeWorkstation.mof >> %TEMP%\mof.log
